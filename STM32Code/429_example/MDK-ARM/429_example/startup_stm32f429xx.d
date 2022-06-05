6080f3ab06a2ee14934fc15adbc3ad99de5733ff
“MIKEHHQ” <392920729@qq.com>
2022-06-05 13:58:15 +0800

    newFolderForSTM32Chips

---
+++ STM32Code/429_example/MDK-ARM/429_example/startup_stm32f429xx.d
@@ -0,0 +1,1 @@
+429_example\startup_stm32f429xx.o: startup_stm32f429xx.s
---
+++ STM32Code/429_example/MDK-ARM/429_example/stm32f4xx_ll_gpio.d
@@ -0,0 +1,12 @@
+429_example\stm32f4xx_ll_gpio.o: ../Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_ll_gpio.c
+429_example\stm32f4xx_ll_gpio.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_ll_gpio.h
+429_example\stm32f4xx_ll_gpio.o: ../Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h
+429_example\stm32f4xx_ll_gpio.o: ../Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f429xx.h
+429_example\stm32f4xx_ll_gpio.o: ../Drivers/CMSIS/Include/core_cm4.h
+429_example\stm32f4xx_ll_gpio.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+429_example\stm32f4xx_ll_gpio.o: ../Drivers/CMSIS/Include/cmsis_version.h
+429_example\stm32f4xx_ll_gpio.o: ../Drivers/CMSIS/Include/cmsis_compiler.h
+429_example\stm32f4xx_ll_gpio.o: ../Drivers/CMSIS/Include/cmsis_armcc.h
+429_example\stm32f4xx_ll_gpio.o: ../Drivers/CMSIS/Include/mpu_armv7.h
+429_example\stm32f4xx_ll_gpio.o: ../Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h
+429_example\stm32f4xx_ll_gpio.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_ll_bus.h
